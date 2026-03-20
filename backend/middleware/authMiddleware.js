de3130256d71d4c105ea19de86c813a8f6b4f171
Rithika KP <rithikapalanisamy07@gmail.com>
2026-03-20 09:01:52 +0530

    Updated auth middleware and improved README

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
 
 const protect = asyncHandler(async (req, res, next) => {
+    
     let token;
 
     if (
@@ -10,18 +11,19 @@ const protect = asyncHandler(async (req, res, next) => {
         req.headers.authorization.startsWith('Bearer')
     ) {
         try {
-            // Get token from header
             token = req.headers.authorization.split(' ')[1];
 
-            // Verify token
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            // Get user from the token
             req.user = await User.findById(decoded.id).select('-password');
 
             next();
         } catch (error) {
-            console.log(error);
+            if (error.name === 'TokenExpiredError') {
+                res.status(401);
+                throw new Error('Token expired');
+            }
+            console.error(error);
             res.status(401);
             throw new Error('Not authorized');
         }
